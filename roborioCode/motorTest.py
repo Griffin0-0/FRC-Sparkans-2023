a410dc482f588837dd53908c228ecc9651321e75
sir-sandwitch <bodietheburgereater@gmail.com>
2023-01-13 08:41:00 -0700

    update for dual brushed motor

--- roborioCode/motorTest.py
+++ roborioCode/motorTest.py
@@ -1,4 +1,4 @@
-# A script to set RoboRIO motors connected to spark max on CAN to 50% power
+# A script to set RoboRIO motors connected to spark max on CAN to 20% power
 
 
 from rev import CANSparkMax
@@ -8,6 +8,8 @@ MOTOR_TYPE = CANSparkMax.MotorType.kBrushed
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.motor = CANSparkMax(1, MOTOR_TYPE)
+        self.motor = CANSparkMax(3, MOTOR_TYPE)
+        self.motor1 = CANSparkMax(4, MOTOR_TYPE)
     def teleopPeriodic(self):
-        self.motor.set(0.5)
+        self.motor.set(0.2)
+        self.motor1.set(0.2)
